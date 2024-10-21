4f84694d95a240401378cee1e405cb1c6674d781
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-10-21 08:46:10 +0545

    feat: add behavioral/observer

---
+++ behavioral/observer/main.go
@@ -0,0 +1,79 @@
+package main
+
+import "fmt"
+
+type Observer interface {
+	update(string)
+	getID() string
+}
+
+type Subject interface {
+	register(observer Observer)
+	deregister(observer Observer)
+	notifyAll()
+}
+
+type Item struct {
+	observerList []Observer
+	name         string
+	inStock      bool
+}
+
+func newItem(name string) *Item {
+	return &Item{
+		name: name,
+	}
+}
+
+func (i *Item) updateAvailability() {
+	fmt.Printf("Item %s is now in stock\n", i.name)
+	i.inStock = true
+	i.notifyAll()
+}
+
+func (i *Item) register(o Observer) {
+	i.observerList = append(i.observerList, o)
+}
+
+func (i *Item) deregister(o Observer) {
+	i.observerList = removeFromSlice(i.observerList, o)
+}
+
+func (i *Item) notifyAll() {
+	for _, observer := range i.observerList {
+		observer.update(i.name)
+	}
+}
+
+func removeFromSlice(observerList []Observer, observerToRemove Observer) []Observer {
+	for i, observer := range observerList {
+		if observer.getID() == observerToRemove.getID() {
+			return append(observerList[:i], observerList[i+1:]...)
+		}
+	}
+	return observerList
+}
+
+type Customer struct {
+	id string
+}
+
+func (c *Customer) update(itemName string) {
+	fmt.Printf("Sending email to customer %s for item %s\n", c.id, itemName)
+}
+
+func (c *Customer) getID() string {
+	return c.id
+}
+
+func main() {
+	var shirtItem = newItem("Nike Shirt")
+
+	observerFirst := &Customer{id: "abc@gmail.com"}
+	observerSecond := &Customer{id: "xyz@gmail.com"}
+
+	shirtItem.register(observerFirst)
+	shirtItem.register(observerSecond)
+
+	shirtItem.updateAvailability()
+}
