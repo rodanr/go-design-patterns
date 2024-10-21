a2e614c8d2b9aa2678b9c7730f457b90eca62791
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-10-21 10:31:48 +0545

    feat: add behavioral/iterator

---
+++ behavioral/iterator/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+type User struct {
+	name string
+	age  int
+}
+
+type Collection interface {
+	createIterator() Iterator
+}
+
+type UserCollection struct {
+	users []*User
+}
+
+func (u *UserCollection) createIterator() Iterator {
+	return &UserIterator{
+		users: u.users,
+	}
+}
+
+type Iterator interface {
+	hasNext() bool
+	getNext() *User
+}
+
+type UserIterator struct {
+	index int
+	users []*User
+}
+
+func (u *UserIterator) hasNext() bool {
+	return u.index < len(u.users)
+}
+
+func (u *UserIterator) getNext() *User {
+	if u.hasNext() {
+		user := u.users[u.index]
+		u.index++
+		return user
+	}
+	return nil
+}
+
+func main() {
+	user1 := &User{name: "Alice", age: 30}
+	user2 := &User{name: "Bob", age: 25}
+
+	userCollection := &UserCollection{
+		users: []*User{user1, user2},
+	}
+
+	iterator := userCollection.createIterator()
+
+	for iterator.hasNext() {
+		user := iterator.getNext()
+		fmt.Printf("User: %+v\n", user)
+	}
+}
